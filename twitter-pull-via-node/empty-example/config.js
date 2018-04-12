c24ec052a5c7474ea5936ca1625526c976444d20
millenniallglyphs <Home@Calvins-MacBook-Pro.local>
2018-04-11 22:29:16 -0400

    trimming
    clarity

--- twitter-pull-via-node/empty-example/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'qx07bvJo2DDx7pGvmaYAFXFoy',
-  consumer_secret:      'Y9FBNfu9peUbgjI0CALFizWMbQTn9RCO8SCzlkI1ga2JagoAuB',
-  access_token:         '4556700449-DxGNK3vm5PAavefNSHyl1nRRgyk7qPbEbNHrge6',
-  access_token_secret:  'YSgMSkmszey2g5adQbML6t3WaayZeURnEurFqDFU6tG0s',
-}
